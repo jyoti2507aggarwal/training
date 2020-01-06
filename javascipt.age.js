50e64485bac4aa71c497e39b2d3f41bbfc0523f4
jyoti2507aggarwal <jyoti2507aggarwal@gmail.com>
2020-01-06 16:49:00 +0530

    hello github

---
+++ javascipt.age.js
@@ -0,0 +1,6 @@
+var first=prompt("enter first name") 
+var second=prompt ("enter second name")
+var age=prompt ("enter your age")
+
+alert("your fullname is" + first + second);
+console.log("and your age" +age);
